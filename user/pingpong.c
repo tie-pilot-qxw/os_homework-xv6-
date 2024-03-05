05374006c5b28e6cd494d9080689c9206e64388e
QiangXinwei <qiangxinwei@sjtu.edu.cn>
2024-03-04 23:28:48 -0800

    pingpong finished

---
+++ user/pingpong.c
@@ -0,0 +1,43 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+    if(argc != 1) {
+        fprintf(2, "usage: pingpong\n");
+        exit(1);
+    }
+    int p[2];
+    char buf[100];
+    pipe(p);
+    int pid = fork();
+    if(pid > 0){
+        if (write(p[1], "ping", sizeof("ping")) != sizeof("ping")){
+            fprintf(2,"write error\n");
+            exit(1);
+        }
+        if (read(p[0], buf, sizeof("pong")) < 0){
+            fprintf(2,"read error\n");
+            exit(1);
+        }
+        printf("%d:received %s\n", getpid(), buf);
+        pid = wait((int *) 0);
+    } else if(pid == 0){
+        if (read(p[0], buf, sizeof(buf)) < 0) {
+            fprintf(2, "read error\n");
+            exit(1);
+        }
+        printf("%d:received %s\n", getpid(), buf);
+        if (write(p[1], "pong", sizeof("pong")) != sizeof("pong")) {
+            fprintf(2,"write error\n");
+            exit(1);
+        }
+        exit(0);
+    } else {
+        printf("fork error\n");
+    }
+    close(p[0]);
+    close(p[1]);
+    exit(0);
+}
